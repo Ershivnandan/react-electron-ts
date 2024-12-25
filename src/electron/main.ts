774e7bc1e3a3d3c294cefb38617a9d95e2a7cc13
ErShivnandan <ec1912@global.org.in>
2024-12-25 20:05:12 +0530

    preload ts aaded

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,7 @@
 import {app, BrowserWindow, ipcMain} from "electron";
-// we will use "path" because Mac device need forword slashes (/) but window need backword slash (\) to read path 
-import path from "path"
 import { isDev } from "./util.js";
 import { getStaticData, pollResources } from "./resourceManager.js";
-import { getPreloadPath } from "./pathresolver.js";
+import { getPreloadPath, getUIPath } from "./pathresolver.js";
 
 app.on("ready", ()=>{
     const mainWindow = new BrowserWindow({
@@ -17,7 +15,7 @@ app.on("ready", ()=>{
         mainWindow.loadURL("http://localhost:5170/");
     }
     else{
-    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
+    mainWindow.loadFile(getUIPath())
 
     }
 
