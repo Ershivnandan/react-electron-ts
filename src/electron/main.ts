be966a81e85130f872de091bac0ed9dcd4df1654
ErShivnandan <ec1912@global.org.in>
2024-12-25 14:59:41 +0530

    hotmodule reloading feature added

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,11 +1,20 @@
 import {app, BrowserWindow} from "electron";
 // we will use "path" because Mac device need forword slashes (/) but window need backword slash (\) to read path 
 import path from "path"
+import { isDev } from "./util.js";
 
 type test = string;
 
 app.on("ready", ()=>{
     const mainWindow = new BrowserWindow({});
+    mainWindow.webContents.openDevTools();
 
+    if(isDev()){
+        mainWindow.loadURL("http://localhost:5170/");
+    }
+    else{
     mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
+
+    }
+
 })
